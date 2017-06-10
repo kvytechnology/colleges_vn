f81f59233163e9f88e4dbeabd321c110ee8bc101
Long Nguyen <long.polyglot@gmail.com>
2017-06-10 22:38:48 +0700

    Setup sample template

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,46 +1,47 @@
 import React from 'react'
-import { Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-// import 'antd/dist/antd.css'
 
 export default class App extends React.Component {
   state = {
-    current: 'mail',
+    currentPage: 'home',
   }
-  handleClick = (e) => {
-    console.log('click ', e);
-    this.setState({
-      current: e.key,
-    });
-  }
+
   render() {
     return (
-      <Menu
-        onClick={this.handleClick}
-        selectedKeys={[this.state.current]}
-        mode="horizontal"
-      >
-        <Menu.Item key="mail">
-          <Icon type="mail" />Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled>
-          <Icon type="appstore" />Navigation Two
-        </Menu.Item>
-        <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-          <MenuItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </MenuItemGroup>
-          <MenuItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </MenuItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-        </Menu.Item>
-      </Menu>
+      <Layout className="layout">
+        <Header>
+          <Menu
+            selectedKeys={[this.state.currentPage]}
+            mode="horizontal"
+            style={{ lineHeight: '64px' }}
+            theme="dark"
+          >
+            <Menu.Item key="home">
+              <a href="/"><Icon type="home" /> Trang chủ</a>
+            </Menu.Item>
+            <Menu.Item key="about">
+              <a href="/about">Về chúng tôi</a>
+            </Menu.Item>
+          </Menu>
+        </Header>
+
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '12px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        </Content>
+
+        <Footer style={{ textAlign: 'center' }}>
+          KVY Tech @2017
+        </Footer>
+      </Layout>
     );
   }
 }
